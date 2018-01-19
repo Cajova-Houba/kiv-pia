2a518fb0a9894f1eb6f02acaa9d8d6526d459607
Cajova-Houba <tkmushroomer@gmail.com>
2018-01-19 22:47:37 +0100

    Welcome file.

--- src/main/java/cz/zcu/pia/valesz/core/domain/SocialIdentityProvider.java
+++ src/main/java/cz/zcu/pia/valesz/core/domain/SocialIdentityProvider.java
@@ -2,6 +2,7 @@ package cz.zcu.pia.valesz.core.domain;
 
 /**
  * Possible types of social networks which user can use to register.
+ * Not used.
  */
 public enum SocialIdentityProvider {
     NONE,
